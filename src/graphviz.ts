b23527ab9ab64775ffe38657873d2ab1d11dacfe
kinsei0916 <kinsei0916@gmail.com>
2021-05-08 00:34:57 +0900

    Add cluster to graphviz

--- src/graphviz.ts
+++ src/graphviz.ts
@@ -8,6 +8,11 @@ function makeGraph(): Graph {
   g.addNode('World');
   g.addEdge('Hello', 'World');
 
+  const sub = g.addCluster('cluster_0');
+  sub.set('label', 'a');
+  sub.set('fillcolor', '#333333');
+  sub.addEdge('1', '2');
+
   return g;
 }
 
@@ -19,23 +24,26 @@ async function run() {
   const options: RenderOptions = {
     type: 'svg',
     G: {
+      bgcolor: '#222222',
+      fontcolor: '#C6C5FE',
+      fontname: 'Arial',
+      layout: 'dot',
       overlap: false,
       pad: 0.3,
       rankdir: 'LR',
-      layout: 'dot',
-      bgcolor: '#222222',
+      style: 'filled',
     },
     E: {
       color: '#757575',
     },
     N: {
+      color: '#C6C5FE',
+      fontcolor: '#C6C5FE',
       fontname: 'Arial',
       fontsize: '14px',
-      color: '#C6C5FE',
+      height: 0,
       shape: 'box',
       style: 'rounded',
-      height: 0,
-      fontcolor: '#C6C5FE',
     },
   };
 
